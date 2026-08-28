c8474b39565e256f1e3f404269a2838672b40a0e
ayushkumthakur <ayush1503008@gmail.com>
2026-08-28 21:48:45 +0530

    Add Firebase web configuration

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAVpXdYENRDr-tcNhu_aTXr0wtC2q9-jD8",
+  authDomain: "agniveer-vayu-maths.firebaseapp.com",
+  projectId: "agniveer-vayu-maths",
+  storageBucket: "agniveer-vayu-maths.firebasestorage.app",
+  messagingSenderId: "93217072040",
+  appId: "1:93217072040:web:a8ca90f5868b3d2f60339d",
+  measurementId: "G-87N00LHRK3"
+};
